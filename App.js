edd44e565cd4f543509272bba1772661848e2759
Евгений Сергеев <akaevgeniy@yandex.ru>
2022-03-10 11:13:11 +0300

    Fix: navigation lib and init compnents

--- App.js
+++ App.js
@@ -3,11 +3,28 @@ import { StyleSheet } from 'react-native';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import ItemList from './components/ItemList';
+import ItemPage from './components/ItemPage';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
     <Provider store={store}>
-      <ItemList />
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="ItemList">
+          <Stack.Screen
+            name="ItemList"
+            component={ItemList}
+            options={{ title: 'Список' }}
+          />
+          <Stack.Screen
+            name="ItemPage"
+            component={ItemPage}
+            options={{ title: 'Элемент' }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 }
